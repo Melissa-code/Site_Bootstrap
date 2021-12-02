9ee90a78bdafdfe0daaa247c9782eb9bb399b7e5
Melissa Benard <benardmeli@yahoo.fr>
2021-12-02 19:02:39 +0100

    seconde banniere avec carrousel de texte

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
+// Ekko lightbox
+
 $(document).on('click', '[data-toggle="lightbox"]', function(event){
     event.preventDefault();
     $(this).ekkoLightbox();
+})
+
+
+// Carousel de texte
+
+$('.carousel').carousel({
+    interval: 2500,
+    pause: 'null'
 })
